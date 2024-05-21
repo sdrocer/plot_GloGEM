b5d09d4fd7dfebaf8ba2e197bdc33a35202547b8
sdrocer <janosch_beer@gmx.de>
2024-05-21 17:24:59 +0200

    add heatmap & adjust single_point plot

--- plot_output.py
+++ plot_output.py
@@ -4,13 +4,15 @@
     Code written by: Janosch Beer
 """
 
+import os
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-import os
+from matplotlib.dates import YearLocator, DateFormatter
 
-os.chdir("/home/jabeer/code/plot_GloGEM")
+os.chdir("/Users/janoschbeer/Library/Mobile Documents/com~apple~CloudDocs/PhD/Code/plot_GloGEM")
 from read_output import Read_GloGEM # make sure to change the working directory to folder where you code -> os.chdir
 
 # Increase the DPI
@@ -33,7 +35,7 @@ class plot_firnice_temperature:
     """
         Class with various methods to plot the firnice temperature output of GloGEM
     """
-    def single_point(self, dir, depths):
+    def single_point(self, dir, depths, gl_name):
         (temp_data,elevation) = Read_GloGEM.point_firnice_temperature(dir)
 
         # create timestamp from glaciological year
@@ -44,19 +46,21 @@ class plot_firnice_temperature:
 
         # plot different depths
         color_values = np.linspace(0, 1, len(depths))
-        cmap = cm.get_cmap('afmhot_r')
+        cmap = cm.rainbow
         for depth, value in zip(depths, color_values):
             color = cmap(value)
-            plt.plot(temp_data['Timestamp'],temp_data[depth], color=color)
+            label = depth + "m"
+            plt.plot(temp_data['Timestamp'],temp_data[depth], color=color, label=label)
 
         # format
-        plt.ylim(-10,0)
+        plt.ylim(-8,0)
         plt.xlabel("Time [years]")
-        plt.ylabel("14m ice temperature [°C]")
-        # plt.grid()
+        plt.ylabel("Ice temperature [°C]")
+        plt.grid(alpha=0.5)
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5),title="Depth")
+        plt.title("Ice temperatures at " + gl_name + " (" + elevation + " m)")
         plt.tight_layout()
-        plt.title("14m ice temperature for RGI" + dir[-9:-4] + " at " + elevation + " m")
-        plt.savefig("/home/jabeer/products/test_run01/plots/14m_icetemp_" + dir[-9:-4])
+        plt.savefig("/Users/janoschbeer/Library/Mobile Documents/com~apple~CloudDocs/PhD/Code/plot_GloGEM/plots/ice_temp_" + gl_name)
 
     def heatmap(self, dir):
         (temp_data,elevation) = Read_GloGEM.point_firnice_temperature(dir)
@@ -65,22 +69,44 @@ class plot_firnice_temperature:
         temp_data['CalYear'] = glacyear_to_calyear(1979,2019)
         temp_data['Timestamp'] = pd.to_datetime(temp_data['CalYear'].astype(str) + '-' + temp_data['Month'].astype(str), format='%Y-%m')
 
+        # reorganise dataframe to enable visualisation as heatmap
+        temp_data.set_index('Timestamp', inplace=True)
+        temp_data = temp_data.drop(columns=['CalYear','Year','Month'])
+        temp_data.replace(-99.0, np.nan, inplace=True)
+        temp_data_t = temp_data.transpose() # transpose to have depths as columns and timestamp on x axis
+
+
         # create ice temperature heatmap
-        plt.figure(figsize=(10, 4))
+        plt.figure(figsize=(15, 10))
+        plt.imshow(temp_data_t)
+        # sns.heatmap(temp_data_t, cmap='viridis', cbar_kws={'label': 'Ice Temperature (°C)'}, yticklabels=True, vmax=30)
 
-        
+        # Format x-axis to show only the year
+        ax = plt.gca()  
+        ax.xaxis.set_major_locator(YearLocator(1)) ## calling the locator for the x-axis
+        ax.xaxis.set_major_formatter(DateFormatter("%Y")) ## calling the formatter for the x-axis
+
+        plt.xticks(rotation=45, ha='right')
+        plt.savefig("/Users/janoschbeer/Library/Mobile Documents/com~apple~CloudDocs/PhD/Code/plot_GloGEM/plots/heatmap_01450.png")        
 
 
+## Plotting
 
-# Plotting
+# set dir & depths
+dir = "/Users/janoschbeer/Library/Mobile Documents/com~apple~CloudDocs/PhD/data/GloGEM/firnice_temperature/temp_ID1_01450.dat"
+depths = ['3','5','9','14','24','34']
+
 IceTempPlot = plot_firnice_temperature()
 
 # Example glaciers
-IceTempPlot.single_point("/home/jabeer/products/test_run01/CentralEurope/PAST/firnice_temperature/temp_ID1_01450.dat", ['1','2','3','4']) # Aletsch
-IceTempPlot.single_point("/home/jabeer/products/test_run01/CentralEurope/PAST/firnice_temperature/temp_ID1_02822.dat") # Grenzgletscher
+IceTempPlot.single_point(dir, ['3','5','9','14','24','34'],"Aletsch glacier") # Aletsch
 
-# Glacier Candidated (according to Doctoral Plan)
-IceTempPlot.single_point()
-
-dir = "/home/jabeer/products/test_run01/CentralEurope/PAST/firnice_temperature/temp_ID1_01450.dat"
-depths = ['1','2','3','4','5','6','7']
+# Glacier Candidates (according to Doctoral Plan)
+IceTempPlot.single_point() # Chessjengletscher
+IceTempPlot.single_point() # Hohlaubgletscher at Saas Grund
+IceTempPlot.single_point() # Sex Rouge
+IceTempPlot.single_point() # Glacier de Tortin
+IceTempPlot.single_point() # Milibachgletscher
+IceTempPlot.single_point() # Corvatsch
+IceTempPlot.single_point() # Triftjigletscher at Gornergrat
+IceTempPlot.single_point() # Alphubel South
